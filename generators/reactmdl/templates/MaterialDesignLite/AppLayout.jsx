4519b02bb66b7d2941cc957d3215a88a4de24020
Jeffrey <jrhicks@cteh.com>
2016-01-19 10:43:59 -0600

    mdllite react integration

--- generators/reactmdl/templates/MaterialDesignLite/AppLayout.jsx
+++ generators/reactmdl/templates/MaterialDesignLite/AppLayout.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 class Layout extends React.Component {
 
+  componentDidMount() {
+    window.componentHandler.upgradeDom();
+  }
+
+  componentWillUpdate() {
+    window.componentHandler.upgradeDom();
+  }
+
   render() {
     return (
       <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
